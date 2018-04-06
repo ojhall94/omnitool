97f2d330032043a2d7332dfe81f5e4a728c25eb0
Oliver Hall <ojh251@student.bham.ac.uk>
2018-04-06 11:35:16 +0100

    Added setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='omnitool',
+    version='0.1',
+    description='A series of classes for photometric and asteroseismic analysis',
+    url='https://github.com/ojhall94/omnitool',
+    author='Oliver James Hall',
+    author_email='ojh251@student.bham.ac.uk',
+    license='MIT',
+    packages=['omnitool']
+    install_requires=[
+        'matplotlib'
+        'numpy'
+        ]
+    zip_safe=False)
